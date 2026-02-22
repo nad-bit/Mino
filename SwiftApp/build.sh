d41fff81c66e8c29786d4e3cd998dd28706e7d68
NAD <nadied@yandex.com>
2026-02-22 16:32:42 +0100

    chore: bump version to 0.9.0 (169) for inline menu actions

--- SwiftApp/build.sh
+++ SwiftApp/build.sh
@@ -32,9 +32,9 @@ cat > "$CONTENTS_DIR/Info.plist" <<EOF
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>0.8.4</string>
+    <string>0.9.0</string>
     <key>CFBundleVersion</key>
-    <string>168</string>
+    <string>169</string>
     <key>LSMinimumSystemVersion</key>
     <string>12.0</string>
     <key>LSUIElement</key>
